2a936a5b38d9ee7818a355a25e63d5356c76ac8e
GameDevRafael <rgtdfaustino@gmail.com>
2025-01-25 23:53:52 +0000

    Refactor cookie banner script to use URL constructor for GTM and GA script sources

--- js/cookie-banner.js
+++ js/cookie-banner.js
@@ -25,21 +25,16 @@ const CONSENT_CONFIG = {
                         if (!manager.hasConsent('analytics')) return;
                        
                         const CLOUDFLARE_ANALYTICS_PROXY = 'https://secretkeys.contact-studentx.workers.dev';
-                        
-                        window.dataLayer = window.dataLayer || [];
-                        window.gtag = function() { dataLayer.push(arguments); }
-                       
+
                         // Proxy for Google Tag Manager script
                         const gtmProxyScript = document.createElement('script');
                         gtmProxyScript.async = true;
-                        gtmProxyScript.src = `${CLOUDFLARE_ANALYTICS_PROXY}/gtm.js`;
-                        document.head.appendChild(gtmProxyScript);
-           
+                        gtmProxyScript.src = new URL('/gtm.js', CLOUDFLARE_ANALYTICS_PROXY).href;
+                        
                         // Proxy for Google Analytics script
                         const gtagProxyScript = document.createElement('script');
                         gtagProxyScript.async = true;
-                        gtagProxyScript.src = `${CLOUDFLARE_ANALYTICS_PROXY}/gtag/js`;
-                        document.head.appendChild(gtagProxyScript);
+                        gtagProxyScript.src = new URL('/gtag/js', CLOUDFLARE_ANALYTICS_PROXY).href;
            
                         gtagProxyScript.onload = function() {
                             gtag('js', new Date());
