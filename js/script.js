4810500c8b568cd8591affbe7ebe85fa9d6c21bf
Rafael Galveia Taveira David Faustino <rgtdfaustino@gmail.com>
2025-07-06 03:21:05 +0100

    teste1Legit

--- js/script.js
+++ js/script.js
@@ -180,7 +180,7 @@ document.addEventListener("DOMContentLoaded", function() {
         startMessageRotation('textContainerOpportunities', opportunitiesMessages, 'animated-text-opportunities');
     }
 
-    cleanupOldPreferences();
+    performMaintenanceCleanup();
     
     // now at the end we start the carousel
     initializeCarousel();
